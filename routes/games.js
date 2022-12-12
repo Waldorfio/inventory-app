29c9d30d57f058d08504610fe75f08d54d42059b
Walid VM <walid.hanifi707@gmail.com>
2022-12-12 20:09:23 +1000

    Initial commit

---
+++ routes/games.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+// Require controllers
+//*to-do
+
+// GAME ROUTES
+
+// PUBLISHER ROUTES
+
+// PLATFORM ROUTES
+
+
+module.exports = router;
+
+
