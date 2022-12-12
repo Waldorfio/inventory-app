0bc0f62b439f85ed5008a3fb3056901d72e72317
Walid VM <walid.hanifi707@gmail.com>
2022-12-12 22:38:30 +1000

    Add boilerplate and TODO's

---
+++ controllers/platformController.js
@@ -0,0 +1,18 @@
+const Game = require('../models/game');
+const Platform = require('../models/platform');
+
+const { body, validationResult } = require('express-validator');
+const async = require('async');
+
+// TODO: platform_create_get (display form to create platform)
+// TODO: platform_create_post
+
+// TODO: platform_update_get (display form to update platform)
+// TODO: platform_update_post
+
+// TODO: platform_delete_get (display form to delete platform)
+// TODO: platform_delete_post
+
+// TODO: platform_list (display list of all platforms)
+
+// TODO: platform_detail (display detail for one platform)
---
+++ controllers/publisherController.js
@@ -0,0 +1,18 @@
+const Game = require('../models/game');
+const Publisher = require('../models/publisher');
+
+const { body, validationResult } = require('express-validator');
+const async = require('async');
+
+// TODO: publisher_create_get (display form to create publisher)
+// TODO: publisher_create_post
+
+// TODO: publisher_update_get (display form to update publisher)
+// TODO: publisher_update_post
+
+// TODO: publisher_delete_get (display form to delete publisher)
+// TODO: publisher_delete_post
+
+// TODO: publisher_list (display list of all publishers)
+
+// TODO: publisher_detail (display detail for one publisher)
