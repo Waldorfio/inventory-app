a0b3f6c27790f7944d54c2e53782d9f01109223f
Walid VM <walid.hanifi707@gmail.com>
2022-12-12 22:35:11 +1000

    Add boilerplate and TODO's

---
+++ controllers/gameController.js
@@ -0,0 +1,22 @@
+const Game = require('../models/game');
+const Platform = require('../models/platform');
+const Publisher = require('../models/publisher');
+
+const { body, validationResult } = require('express-validator');
+const async = require('async');
+
+// TODO: exports.index
+
+
+// TODO: game_create_get (display form to create game)
+// TODO: game_create_post
+
+// TODO: game_update_get (display form to update game)
+// TODO: game_update_post
+
+// TODO: game_delete_get (display form to delete game)
+// TODO: game_delete_post
+
+// TODO: game_list (display list of all games)
+
+// TODO: game_detail (display detail for one game)
