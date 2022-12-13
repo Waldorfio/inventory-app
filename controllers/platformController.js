4a91c021a1e68d828abe7403dbacb622216e69ce
Walid VM <walid.hanifi707@gmail.com>
2022-12-13 21:51:33 +1000

    Add TODO's for CRUD operation functions

--- controllers/platformController.js
+++ controllers/platformController.js
@@ -4,15 +4,46 @@ const Platform = require('../models/platform');
 const { body, validationResult } = require('express-validator');
 const async = require('async');
 
-// TODO: platform_create_get (display form to create platform)
-// TODO: platform_create_post
 
-// TODO: platform_update_get (display form to update platform)
+// TODO: Display form to create platform
+exports.platform_create_get = function (req, res, next) {
+    res.render('platform_form');
+};
+// TODO: Handle form data from above
+exports.platform_create_post = [
+    body('Platform Name', 'Invalid Name') // TODO: crosscheck name field with forms name field
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+]
+
+// TODO: Display form to update platform
+exports.platform_update_get = function (req, res, next) {
+    // TODO: Search for platform if exists, display error if not, otherwise...
+    // TODO: Need to render the same platform_form, but this time with the title "Update form".
+}
 // TODO: platform_update_post
+exports.platform_update_post = [
+    body('Platform Name', 'Invalid Name') // TODO: crosscheck name field with forms name field
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+]
 
-// TODO: platform_delete_get (display form to delete platform)
+// TODO: Display form to delete platform
+exports.platform_delete_get = function (req, res, next) {
+}
 // TODO: platform_delete_post
+exports.platform_delete_post = [
 
-// TODO: platform_list (display list of all platforms)
+]
 
-// TODO: platform_detail (display detail for one platform)
+// TODO: Display list of all platforms
+exports.platform_list = function (req, res, next) {
+    Game.find()
+}
+
+// TODO: Display detail for one platform
+exports.platform_detail = function (req, res, next) {
+    
+}
