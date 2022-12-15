adee1bbea3de7bcfb4c3c9b8ce03b849f55ab127
Wally <walid.hanifi707@gmail.com>
2022-12-15 15:41:03 +1000

    Add boilerplate async crud functions

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -6,17 +6,58 @@ const { body, validationResult } = require('express-validator');
 const async = require('async');
 
 // TODO: exports.index
+const index = async (req, res, next) => {
+  try {
+    const game_list = await Game.find();
+    console.log('List of all games: '+game_list);
+  } catch(err) {
+    console.error(err);
+}
 
+const game_create = async (req, res) => {
+  try {
+    await Game.create({
+      name: req.body.name, // TODO: Add other document fields for game
+    })
+    console.log('Game created! ('+req.body.name+')');
+  } catch(err) {
+    console.error(err);
+  }   
+}
 
-// TODO: game_create_get (display form to create game)
-// TODO: game_create_post
+const game_read = async (req, res) => {
+  try {
+    await Game.find() // TODO: Fix find function
+  } catch(err) {
+    console.error(err);
+  }
+}
 
-// TODO: game_update_get (display form to update game)
-// TODO: game_update_post
+const game_update = async (req, res) => {
+  try {
+    await Game.findByIdAndUpdate(
+      { _id: id },
+      { $set: { name: req.body.name } }  // TODO: Clarify
+    )
+    console.log('Game updated! ('+req.body.name+')');
+  } catch(err) {
+    console.error(err);
+  }
+}
 
-// TODO: game_delete_get (display form to delete game)
-// TODO: game_delete_post
+const game_destroy = async (req, res) => {
+  try {
+    await Publisher.findByIdAndDelete(req.params.id);
+    console.log('Game deleted! ('+req.params.id+')');
+  } catch(err) {
+    console.error(err);
+  }
+}
 
-// TODO: game_list (display list of all games)
-
-// TODO: game_detail (display detail for one game)
+module.exports = {
+  index,
+  game_create,
+  game_read,
+  game_update,
+  game_destroy,
+}
