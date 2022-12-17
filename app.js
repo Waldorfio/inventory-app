322844093d6f4ad284590d9e7bbab1bedc377749
Walid VM <walid.hanifi707@gmail.com>
2022-12-17 20:28:44 +1000

    Fix routing of mainRouter

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/mainRouter', mainRouter); // *Added main router
+app.use('/', mainRouter); // *Added main router
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
