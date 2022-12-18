cb2310c04a4bad11b7a9e3146ecf45b992781d8b
Walid VM <walid.hanifi707@gmail.com>
2022-12-18 15:09:35 +1000

    Update mongodb url security

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(function(err, req, res, next) {
 // Set up mongoose connection
 const mongoose = require("mongoose");
  // *Added dotenv protected url
-const mongoDB = 'mongodb+srv://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD+'@cluster0.0lvrsmq.mongodb.net/?retryWrites=true&w=majority'
+const mongoDB = 'mongodb+srv://'+process.env.DB_USERNAME+':'+process.env.DB_PASSWORD+'@cluster0.0lvrsmq.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
