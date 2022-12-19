6c8cf96807812c419882afc13b493b0978480d6c
Walid VM <walid.hanifi707@gmail.com>
2022-12-19 17:23:30 +1000

    Fix closure

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -9,12 +9,12 @@ const publisherController = require('../controllers/publisherController');
 // GAME ROUTES
 router.get('/games', gameController.index); // index
 // CREATE
-router.get('/game/create', async (req, res) => {res.render('gameform', type: 'Create')}); // CREATE FORM
+router.get('/game/create', async (req, res) => {res.render('gameform', {type: 'Create'})}); // CREATE FORM
 router.post('/game/create', gameController.game_create); // CREATE  // TODO ADD PLATFORM AND PUBLISHER DROPDOWN, and RE-ENABLE MODEL
 // READ
 router.get('/game/:id', gameController.game_read); // READ
 // UPDATE
-router.get('/game/:id/update', async (req, res) => {res.render('gameform', type: 'Update')}); // UPDATE FORM
+router.get('/game/:id/update', async (req, res) => {res.render('gameform', {type: 'Update'})}); // UPDATE FORM
 router.post('/game/:id/update', gameController.game_update); // UPDATE
 // DESTROY
 router.post('/game/:id/delete', gameController.game_destroy); // DESTROY
