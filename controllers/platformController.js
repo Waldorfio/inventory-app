f6492670f9560932cec8a07fe02f4f6eca9618e9
Wally <walid.hanifi707@gmail.com>
2022-12-20 10:28:30 +1000

    Add remaining platform crud operations

--- controllers/platformController.js
+++ controllers/platformController.js
@@ -4,50 +4,91 @@ const Platform = require('../models/platform');
 const { body, validationResult } = require('express-validator');
 const async = require('async');
 
+// Render platforms page
+const platforms_page = async (req, res, next) => {
+    try {
+        const platform_list = await Platform.find();
+        res.render('platforms', {
+            platforms: platform_list,
+        });
+    } catch(err) {
+        console.error(err);
+    }
+}
 
-const platform_create = async (req, res) => {
+// CREATE
+const game_create_get = async (req, res) => {
+  try {
+    res.render('platformform', {
+        action: '/platform/create',
+        name: 'Platform Example',
+    })};
+  } catch(err) {
+    console.error(err);
+  }   
+}
+const platform_create_post = async (req, res) => {
   try {
     await Platform.create({
       name: req.body.name,
     })
-    console.log('Platform created! ('+req.body.name+')');
+    console.log('Platform created!');
+    res.redirect('/');
   } catch(err) {
     console.error(err);
   }   
 }
 
+// READ
 const platform_read = async (req, res) => {
   try {
-    await Platform.find()
+    const foundPlatform = await Platform.findById(req.params.id);
+    res.render('platformform', {
+        action: '/platform/'+foundPlatform.id,
+        name: foundPlatform.name,
+    })
   } catch(err) {
     console.error(err);
   }
 }
 
+// UPDATE
 const platform_update = async (req, res) => {
   try {
     await Platform.findByIdAndUpdate(
-      { _id: id },
-      { $set: { name: req.body.name } }
+      { _id: req.params.id },
+      { name: req.body.name }
     )
-    console.log(' Platform updated! ('+req.body.name+')');
+    console.log(' Platform updated!');
+    res.redirect('/');
   } catch(err) {
     console.error(err);
   }
 }
 
-const platform_destroy = async (req, res) => {
+// DESTROY
+const platform_destroy_get = async (req, res) => {
+    try {
+        res.render('deleteplatform');
+    } catch(err) {
+        console.error(err);
+    }
+}
+const platform_destroy_post = async (req, res) => {
   try {
     await Platform.findByIdAndDelete(req.params.id);
-    console.log('Platform deleted! ('+req.params.id+')');
+    console.log('Platform deleted!');
+    res.redirect('/');
   } catch(err) {
     console.error(err);
   }
 }
 
 module.exports = {
-  platform_create,
+  platform_create_get,
+  platform_create_post,
   platform_read,
   platform_update,
-  platform_destroy,
+  platform_destroy_get,
+  platform_destroy_post,
 }
