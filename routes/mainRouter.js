fc2e24fab8dc8c50986ed08b5b2cd4f9a48e4370
Wally <walid.hanifi707@gmail.com>
2022-12-20 10:22:11 +1000

    Add platform routing

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -6,10 +6,10 @@ const gameController = require('../controllers/gameController');
 const platformController = require('../controllers/platformController');
 
 // GAME ROUTES
-router.get('/games', gameController.index); // index
+router.get('/games', gameController.games_page); // render games page
 // CREATE
 router.get('/game/create', gameController.game_create_get); // redirect to game create form
-router.post('/game/create', gameController.game_create_post); // TODO ADD PLATFORM AND PUBLISHER DROPDOWN, and RE-ENABLE MODEL
+router.post('/game/create', gameController.game_create_post); // TODO ADD PLATFORM DROPDOWN, and RE-ENABLE MODEL
 // READ
 router.get('/game/:id', gameController.game_read);
 // UPDATE
@@ -19,9 +19,16 @@ router.get('/game/:id/delete', gameController.game_destroy_get); // redirect to 
 router.post('/game/:id/delete', gameController.game_destroy_post); // process delete.js submit button
 
 // PLATFORM ROUTES
-router.post('./platform/create', platformController.platform_create); // CREATE
-router.get('./platform/:id', platformController.platform_read); // READ
-router.post('./platform/:id/update', platformController.platform_update); // UPDATE
-router.post('./platform/:id', platformController.platform_destroy); // DESTROY
+router.get('/platforms', platformController.platforms_page); // render platforms page
+// CREATE
+router.get('/platform/create', platformController.platform_create_get); // redirect to platform create form
+router.post('/platform/create', platformController.platform_create_post);
+// READ
+router.get('/platform/:id', platformController.platform_read);
+// UPDATE
+router.post('/platform/:id', platformController.platform_update);
+// DESTROY
+router.get('/platform/:id/delete', platformController.platform_destroy_get); // redirect to delete page, asking to confirm deletion
+router.post('/platform/:id/delete', platformController.platform_destroy_post); // process delete.js submit button
 
 module.exports = router;
